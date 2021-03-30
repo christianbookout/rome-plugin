795b025bad5b68fb670257d828fd46e2f2299b65
intrnlerr <nulltestb1@gmail.com>
2021-03-30 06:40:29 +0000

    ability to save titles to disk

--- src/romeplugin/title/RomeTitles.java
+++ src/romeplugin/title/RomeTitles.java
@@ -1,7 +1,9 @@
 package romeplugin.title;
 
 import org.bukkit.Server;
+import romeplugin.database.StorageInterface;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -34,5 +36,30 @@ public class RomeTitles {
 
     public void setPlayerTitle(UUID uuid, String title) {
         playerTitles.put(uuid, getKnownTitle(title));
+    }
+
+    private void saveData(DataOutputStream stream) {
+        // save playerdata to stream
+        playerTitles.forEach((id,title)-> {
+            try {
+                stream.writeLong(id.getMostSignificantBits());
+                stream.writeLong(id.getLeastSignificantBits());
+                stream.writeUTF(title.name);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void loadData(DataInputStream stream) {
+        try {
+            while (true) {
+                UUID id = new UUID(stream.readLong(), stream.readLong());
+                String titleName = stream.readUTF();
+                playerTitles.put(id, getKnownTitle(titleName));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

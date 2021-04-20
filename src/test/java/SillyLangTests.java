d8325f7ff66b05ab57a5ccfa1e4b9d83a0359fd0
intrnlerr <nulltestb1@gmail.com>
2021-04-20 01:21:14 +0000

    unit testing

--- src/test/java/SillyLangTests.java
+++ src/test/java/SillyLangTests.java
@@ -4,28 +4,40 @@ import romeplugin.sillylang.Interpreter;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import static org.junit.Assert.assertEquals;
+
 public class SillyLangTests {
     Interpreter interpreter = new Interpreter();
 
-    private InputStream stois(String s) {
+    private InputStream sToIs(String s) {
         return new ByteArrayInputStream(s.getBytes());
     }
 
     @Test
     public void testI32() {
-        interpreter.interpret(stois("i32$10 __print "));
+        interpreter.interpret(sToIs("i32$10"));
+        assertEquals(10, interpreter.pop().getValue());
     }
 
     @Test
     public void testBuiltinClass() {
-        interpreter.interpret(stois("\"java.lang.System\" __class __print "));
-        interpreter.interpret(stois("\"hi world\" i32$0 \"out\" \"java.lang.System\" __class __field __field_get \"java.lang.String\" __class i8$1 \"println\" \"java.io.PrintStream\" __class __method __execute "));
+        interpreter.interpret(sToIs("\"java.lang.System\" __class __duplicate __print"));
+        assertEquals(System.class, interpreter.pop().getValue());
+        interpreter.interpret(sToIs("\"hi world\" i32$0 \"out\" \"java.lang.System\" __class __field __field_get \"java.lang.String\" __class i8$1 \"println\" \"java.io.PrintStream\" __class __method __execute"));
     }
 
     @Test
     public void testPrint() {
-        System.out.println("test11");
-        interpreter.interpret(stois("\"yo vivo en vivo\" __print "));
-        interpreter.interpret(stois("\"yo vivo en vivo\" __duplicate __print __print "));
+        interpreter.interpret(sToIs("\"yo vivo\" __print"));
+        interpreter.interpret(sToIs("\"yo vivo\" __duplicate __print __print"));
+    }
+
+
+    @Test
+    public void testSwap() {
+        interpreter.clear();
+        interpreter.interpret(sToIs("i32$1 i32$20 __swap"));
+        assertEquals(1, interpreter.pop().getValue());
+        assertEquals(20, interpreter.pop().getValue());
     }
 }

2434b7a216f51b759c16b689e7d5b89208623f2c
intrnlerr <nulltestb1@gmail.com>
2021-04-24 07:03:53 +0000

    builtin compile-time optimizations maybe :)

--- src/main/java/romeplugin/sillylang/builtin/Builtin.java
+++ src/main/java/romeplugin/sillylang/builtin/Builtin.java
@@ -1,5 +1,6 @@
 package romeplugin.sillylang.builtin;
 
+import romeplugin.sillylang.Subroutine;
 import romeplugin.sillylang.types.SillyType;
 
 import java.util.Deque;
@@ -9,6 +10,14 @@ abstract public class Builtin {
     public void register(Map<String, Builtin> builtins) {
         builtins.put(getId(), this);
     }
+
     protected abstract String getId();
+
+    // compile-time optimizations
+    public void compileExecute(Subroutine routine) {
+        routine.add(this);
+    }
+
+    // runtime execution
     public abstract void execute(Deque<SillyType> stack);
 }

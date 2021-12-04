adc3ad844a967791c6f7f0d43d2ea123c8a462ef
intrnlerr <nulltestb1@gmail.com>
2021-12-04 23:08:13 +0000

    fix player chat

--- src/main/java/romeplugin/EventListener.java
+++ src/main/java/romeplugin/EventListener.java
@@ -5,11 +5,6 @@
  */
 package romeplugin;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,27 +12,18 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import romeplugin.database.SQLConn;
-import romeplugin.title.RomeTitles;
 import romeplugin.newtitle.Title;
 
 /**
  * @author chris
  */
 public class EventListener implements Listener {
-    private final RomeTitles titles;
-
-    public EventListener(RomeTitles titles) {
-        this.titles = titles;
-    }
-    //@EventHandeler
-    //public void doSomethingOnInteract(PlayerInteractEvent e) {}
-
     @EventHandler
     public void handlePlayerChat(AsyncPlayerChatEvent event) {
-        Title playerTitle = titles.getPlayerTitle(event.getPlayer().getUniqueId());
+        Title playerTitle = RomePlugin.onlinePlayers.get(event.getPlayer());
         if (playerTitle != null) {
             // intercept chat message here
-            event.setFormat("[" + playerTitle.color + playerTitle.name + ChatColor.WHITE + "] " + event.getFormat());
+            event.setFormat("[" + playerTitle.toString() + ChatColor.WHITE + "] " + event.getFormat());
         }
     }
 

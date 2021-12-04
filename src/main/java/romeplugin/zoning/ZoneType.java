398e39339da6f1d2dd32a68247cb0a7e13097aa6
angryfirewizard <christianmbookout@gmail.com>
2021-12-04 13:25:29 -0800

    created zone enum

---
+++ src/main/java/romeplugin/zoning/ZoneType.java
@@ -0,0 +1,24 @@
+package romeplugin.zoning;
+
+import romeplugin.newtitle.Title;
+import static romeplugin.newtitle.Title.*;
+
+import java.util.Arrays;
+
+public enum ZoneType {
+    GOVERNMENT(CONSOLE, MAYOR, BUILDER), CITY(MAYOR, BUILDER), SUBURB(), WILDERNESS();
+
+    private Title[] titles;
+    private ZoneType(Title... titles) {
+        this.titles = titles;
+    }
+
+    public Title[] getTitles() {
+        return titles;
+    }
+
+    public boolean canBuild(Title title) {
+        if (this == WILDERNESS) return true;
+        return Arrays.stream(this.titles).anyMatch(title::equals);
+    }
+}

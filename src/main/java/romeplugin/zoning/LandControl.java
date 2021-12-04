b64075e338c1024eb2381f583dc1171735fdd179
angryfirewizard <christianmbookout@gmail.com>
2021-12-04 13:25:53 -0800

    circle -> square

--- src/main/java/romeplugin/zoning/LandControl.java
+++ src/main/java/romeplugin/zoning/LandControl.java
@@ -1,48 +1,61 @@
 package romeplugin.zoning;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+
+import romeplugin.RomePlugin;
+import romeplugin.database.SQLConn;
+import romeplugin.newtitle.Title;
+
+import static romeplugin.zoning.ZoneType.*;
 
 public class LandControl {
-    private int cityX = 0;
-    private int cityY = 0;
-    private int citySize = 0;
 
-    // TODO: don't hardcode this
-    private final Ring[] rings = {
-            new Ring(10, "A"),
-            new Ring(20, "B")
-    };
+    private Square[] rings;
+    private int cityX, cityY;
+    private int initialSize;
+
+    public LandControl(int cityX, int cityY, int initialSize, int cityMult, int suburbsMult) {
+        this.cityX= cityX;
+        this.cityY = cityY;
+        this.initialSize = initialSize;
+        this.rings = new Square[] {
+            new Square(initialSize, GOVERNMENT),
+            new Square(initialSize*cityMult, CITY),
+            new Square(initialSize*suburbsMult, SUBURB)
+        };
+    }
 
     public void setCenter(int x, int y) {
         this.cityX = x;
         this.cityY = y;
     }
 
-    public void setCitySize(int citySize) {
-        this.citySize = citySize;
-    }
+    /*public void setCitySize(int citySize) {
+        this.initialSize = citySize;
+        //TODOLATER :)
+    }*/
 
-    public Ring getRing(double dist) {
-        // TODO: figure out who can break blocks in a ring
-        for (Ring ring : rings) {
-            if (ring.blocksFromCenter < dist) {
-                return ring;
-            }
+    public Square getRing(int x, int y) {
+        for (Square square : rings) {
+            if (x <= square.getSize() + cityX && y <= square.getSize() + cityY) return square;
         }
         return null;
     }
 
-    public double distToCity(int x, int y) {
+    /*public double distToCity(int x, int y) {
         var x_dist = cityX - x;
         var y_dist = cityY - y;
         return Math.sqrt(x_dist * x_dist + y_dist * y_dist);
-    }
+    }*/
 
-    public boolean canBreak(int x, int y) {
-        var dist = distToCity(x, y);
-        if (dist > citySize) {
-            return true;
-        }
-        return getRing(dist).name.equals("B");
+    public boolean canBreak(Player player, int x, int y) { //TODO implement a player cache
+        Title title;
+        if ((title = RomePlugin.onlinePlayers.get(player)) == null) return false;
+        return getRing(x, y).getType().canBuild(title);
     }
 }
--- src/main/java/romeplugin/zoning/LandEventListener.java
+++ src/main/java/romeplugin/zoning/LandEventListener.java
@@ -14,6 +14,10 @@ public class LandEventListener implements Listener {
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         var block = event.getBlock();
-        event.setCancelled(!controller.canBreak(block.getX(), block.getY()));
+        if (controller.canBreak(event.getPlayer(), block.getX(), block.getY())) 
+            return;
+
+        event.getPlayer().sendMessage("you can't break that, dumbass");
+        event.setCancelled(true);
     }
 }

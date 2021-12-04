4a68f25a702cd856e037352b2341e8c1ea9b4753
angryfirewizard <christianmbookout@gmail.com>
2021-12-04 13:22:28 -0800

    added playerjoin event

--- src/main/java/romeplugin/EventListener.java
+++ src/main/java/romeplugin/EventListener.java
@@ -5,12 +5,20 @@
  */
 package romeplugin;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import romeplugin.database.SQLConn;
 import romeplugin.title.RomeTitles;
-import romeplugin.title.Title;
+import romeplugin.newtitle.Title;
 
 /**
  * @author chris
@@ -30,6 +38,25 @@ public class EventListener implements Listener {
         if (playerTitle != null) {
             // intercept chat message here
             event.setFormat("[" + playerTitle.color + playerTitle.name + ChatColor.WHITE + "] " + event.getFormat());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String sQLString = "SELECT * FROM players WHERE uuid = " + event.getPlayer().getUniqueId().toString();
+        try (ResultSet results = SQLConn.read(sQLString)) {
+            if (results == null) {
+                //TODO store player in database
+            }
+
+            String title = results.getString("title");
+            if (title == null) {
+                System.err.println("can't create or find " + event.getPlayer().getName() + "'s title in the database :(");
+            }
+
+            RomePlugin.onlinePlayers.put(event.getPlayer(), Title.getTitle(title));
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }

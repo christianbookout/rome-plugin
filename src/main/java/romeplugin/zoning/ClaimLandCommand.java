2199825fc98230420dd75a3f19b59df7dc48bb01
intrnlerr <nulltestb1@gmail.com>
2021-12-05 09:04:45 +0000

    allow claiming in a radius

--- src/main/java/romeplugin/zoning/ClaimLandCommand.java
+++ src/main/java/romeplugin/zoning/ClaimLandCommand.java
@@ -9,6 +9,29 @@ import romeplugin.database.SQLConn;
 public class ClaimLandCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (args.length < 2) {
+            return false;
+        }
+        if (args[0].equals("radius")) {
+            var player = (Player) sender;
+            if (player == null) {
+                return false;
+            }
+            var r = Integer.parseInt(args[1]);
+            var loc = player.getLocation();
+            var x0 = loc.getBlockX() - r;
+            var y0 = loc.getBlockZ() + r;
+            var x1 = loc.getBlockX() + r;
+            var y1 = loc.getBlockZ() - r;
+            var claim = SQLConn.getClaimRect(x0, y0, x1, y1);
+            if (claim != null) {
+                sender.sendMessage("land already claimed >:(");
+                return false;
+            }
+            SQLConn.addClaim(x0, y0, x1, y1, player.getUniqueId());
+            sender.sendMessage("successfully claimed " + (x1 - x0) * (y1 - y0) + " blocks.");
+            return true;
+        }
         if (args.length < 4) {
             return false;
         }

940df6b53854e09e52021a3728683bfd93f2a73a
intrnlerr <nulltestb1@gmail.com>
2021-12-14 04:42:14 +0000

    format

--- src/main/java/romeplugin/messageIntercepter/DistanceListener.java
+++ src/main/java/romeplugin/messageIntercepter/DistanceListener.java
@@ -11,29 +11,29 @@ public class DistanceListener implements Listener {
     private final int distance;
     private final Server server;
     private final boolean doSwearFilter;
-    
+
     public DistanceListener(Server server, int distance, boolean doSwearFilter) {
         this.server = server;
         this.distance = distance;
         this.doSwearFilter = doSwearFilter;
     }
 
-    @EventHandler (priority=EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onMessage(AsyncPlayerChatEvent e) {
         String message = e.getMessage();
-        if (doSwearFilter) 
+        if (doSwearFilter)
             message = SwearFilter.replaceSwears(message);
-        
+
         broadcastMessage(e.getPlayer(), message);
         e.setCancelled(true);
     }
 
     public void broadcastMessage(Player sender, final String message) {
         server.getOnlinePlayers().forEach(
-            p ->  {
-                if (distance == 0 || p.getLocation().distance(sender.getLocation()) <= distance) 
-                    p.sendMessage(sender.getUniqueId(), message);
-            }
+                p -> {
+                    if (distance == 0 || p.getLocation().distance(sender.getLocation()) <= distance)
+                        p.sendMessage(sender.getUniqueId(), message);
+                }
         );
     }
 }

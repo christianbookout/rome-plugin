46180ca70dee5fcf672b7578c1fae62551e9d9dc
angryfirewizard <christianmbookout@gmail.com>
2021-12-14 21:46:38 -0800

    print new zones you enter

---
+++ src/main/java/romeplugin/zoning/LandEnterListener.java
@@ -0,0 +1,24 @@
+package romeplugin.zoning;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class LandEnterListener implements Listener {
+    private LandControl controller;
+
+    public LandEnterListener(LandControl controller) {
+        this.controller = controller;
+    }
+
+    @EventHandler
+    public void onPlayerMoveEvent(PlayerMoveEvent e) {
+        if (e.getTo().getBlockX() == e.getFrom().getBlockX() && e.getTo().getBlockZ() == e.getFrom().getBlockZ()) 
+            return;
+        
+        CityArea newZone = controller.getArea(e.getFrom());
+        if (newZone != null && controller.getArea(e.getTo()).getType() != newZone.getType()) {
+            e.getPlayer().sendMessage("you've entered the " + newZone.getType().toString().toLowerCase() + " zone");
+        }
+    }
+}

53219e34e0d326d8461dd6d384204c80f7a03f75
intrnlerr <nulltestb1@gmail.com>
2021-12-17 05:59:08 +0000

    tell the player they are in the right zone

--- src/main/java/romeplugin/zoning/LandEnterListener.java
+++ src/main/java/romeplugin/zoning/LandEnterListener.java
@@ -16,11 +16,11 @@ public class LandEnterListener implements Listener {
         if (e.getTo() == null) {
             return;
         }
-        if (e.getTo().getBlockX() == e.getFrom().getBlockX() && e.getTo().getBlockZ() == e.getFrom().getBlockZ()) 
+        if (e.getTo().getBlockX() == e.getFrom().getBlockX() && e.getTo().getBlockZ() == e.getFrom().getBlockZ())
             return;
-        
-        CityArea newZone = controller.getArea(e.getFrom());
-        if (newZone != null && controller.getArea(e.getTo()).getType() != newZone.getType()) {
+
+        CityArea newZone = controller.getArea(e.getTo());
+        if (newZone != null && controller.getArea(e.getFrom()).getType() != newZone.getType()) {
             e.getPlayer().sendMessage("you've entered the " + newZone.getType().toString().toLowerCase() + " zone");
         }
     }

aede0ca5b267ef6893f3fc35058082539fa885d0
angryfirewizard <christianmbookout@gmail.com>
2022-01-02 18:14:42 -0800

    add title to player from UUID

--- src/main/java/romeplugin/newtitle/TitleHandler.java
+++ src/main/java/romeplugin/newtitle/TitleHandler.java
@@ -12,9 +12,11 @@ import java.util.UUID;
 
 public class TitleHandler {
     private final PermissionsHandler perms;
+    private final Plugin plugin;
 
     public TitleHandler(Plugin plugin) {
-        perms = new PermissionsHandler(plugin);
+        this.plugin = plugin;
+        this.perms = new PermissionsHandler(plugin);
     }
 
     public void playerJoin(Player player) {
@@ -31,13 +33,33 @@ public class TitleHandler {
         RomePlugin.onlinePlayerTitles.remove(player);
     }
 
+    private void updatePlayer(Player player, Title title) {
+        var oldTitle = RomePlugin.onlinePlayerTitles.put(player, title);
+        perms.updateTitle(player, title, oldTitle);
+    }
+
+    public boolean setTitle(UUID uuid, Title title) {
+        if (!setTitleOffline(uuid, title)) {
+            return false;
+        }
+        Player player = plugin.getServer().getOnlinePlayers()
+            .stream()
+            .filter(p -> p.getUniqueId().equals(uuid))
+            .findFirst()
+            .orElse(null);
+
+        if (player != null) {
+            updatePlayer(player, title);
+            return true;
+        }
+        else return false;
+    }
     public boolean setTitle(Player player, Title title) {
         if (!setTitleOffline(player.getUniqueId(), title)) {
             return false;
         }
 
-        var oldTitle = RomePlugin.onlinePlayerTitles.put(player, title);
-        perms.updateTitle(player, title, oldTitle);
+        updatePlayer(player, title);
         return true;
     }
 

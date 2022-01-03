d9bf507bd857d8a26f45989d14bf2494c20557dd
angryfirewizard <christianmbookout@gmail.com>
2022-01-02 23:20:21 -0800

    add command for censors to start an election

---
+++ src/main/java/romeplugin/election/StartElectionCommand.java
@@ -0,0 +1,29 @@
+package romeplugin.election;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import romeplugin.MessageConstants;
+
+public class StartElectionCommand implements CommandExecutor{
+
+    private final ElectionHandler electionHandler;
+
+    public StartElectionCommand(ElectionHandler electionHandler) {
+        this.electionHandler = electionHandler;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (electionHandler.hasElection()) {
+            sender.sendMessage(MessageConstants.ALREADY_ELECTION_ERROR);
+        }
+        else {
+            electionHandler.startElection();
+            sender.sendMessage(MessageConstants.SUCCESSFUL_ELECTION_START);
+        }
+        return true;
+    }
+    
+}

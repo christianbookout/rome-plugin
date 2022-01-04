8469072af2ca8c24e67b9f970932ecdb905c0f7c
angryfirewizard <christianmbookout@gmail.com>
2022-01-03 21:40:40 -0800

    added messages

--- src/main/java/romeplugin/MessageConstants.java
+++ src/main/java/romeplugin/MessageConstants.java
@@ -7,8 +7,10 @@ public class MessageConstants { //TODO transfer all message strings to this file
     /****************************************** ELECTIONS ******************************************/
 
     public static final String ALREADY_ELECTION_ERROR = ChatColor.RED + "election is already running";
+    public static final String ALREADY_VOTING_ERROR = ChatColor.RED + "the election is already in the voting phase";
     public static final String NO_ELECTION_ERROR = ChatColor.RED + "there is no election at the moment";
-    public static final String SUCCESSFUL_ELECTION_START = "election successfully started";
+    public static final String SUCCESSFUL_VOTING_START = "voting period started";
+    public static final String SUCCESSFUL_ELECTION_START = "election period started. use /run to run for a position";
     public static final String CANT_FIND_PLAYER = ChatColor.RED + "can't find player named " + ChatColor.ITALIC;
     public static final String CANT_FIND_TITLE = ChatColor.RED + "can't find title named " + ChatColor.ITALIC;
     public static final String ELECTION_ENDED = "election ended! use /results to view the results";

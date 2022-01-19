84228fb1c28d75e41e7d8c99a732ed25ad5b2c37
intrnlerr <nulltestb1@gmail.com>
2022-01-19 07:01:28 +0000

    add a tabcompleter for /elections

---
+++ src/main/java/romeplugin/election/ElectionTabCompleter.java
@@ -0,0 +1,51 @@
+package romeplugin.election;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import romeplugin.title.Title;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ElectionTabCompleter implements TabCompleter {
+    private final String[] subCommands = {
+            "run",
+            "quit",
+            "help",
+            "results",
+            "candidates",
+            "vote",
+            "seevotes",
+            "phase",
+            "start",
+            "voting",
+            "end",
+            "cancel"
+    };
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args) {
+        if (args.length == 0) {
+            return Arrays.asList(subCommands);
+        } else if (args.length == 1) {
+            var complete = Arrays.asList(subCommands);
+            complete.removeIf(str -> !str.startsWith(args[0]));
+            return complete;
+        } else if (args.length == 3) {
+            switch (args[1]) {
+                case "run":
+                    return Arrays.stream(Title.values())
+                            .map(Enum::toString)
+                            .filter(title -> title.startsWith(args[2]))
+                            .collect(Collectors.toList());
+                case "vote":
+                    // return default list of online players
+                    return null;
+            }
+        }
+        return Collections.emptyList();
+    }
+}

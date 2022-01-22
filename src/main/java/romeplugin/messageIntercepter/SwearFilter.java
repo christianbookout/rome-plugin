c908e2b1f24a6af9027bad6d5dd454e9e2729490
intrnlerr <nulltestb1@gmail.com>
2022-01-22 21:52:27 +0000

    use constructor for the purpose of constructing

--- src/main/java/romeplugin/messageIntercepter/SwearFilter.java
+++ src/main/java/romeplugin/messageIntercepter/SwearFilter.java
@@ -1,30 +1,31 @@
 package romeplugin.messageIntercepter;
 
+import org.bukkit.entity.Player;
+import romeplugin.zoning.claims.LandControl;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import org.bukkit.entity.Player;
-
-import romeplugin.zoning.claims.LandControl;
 
 public final class SwearFilter {
-
     private static final InputStream SWEAR_STREAM = SwearFilter.class.getResourceAsStream("/swears.csv");
-    private static final ArrayList<String> swears = new ArrayList<>();
+    private static final char SWEAR_REPLACE = '♥';
+    private final ArrayList<String> swears = new ArrayList<>();
     private final LandControl controller;
-    
+
     //0 for off, 1 for only city/government, 2 for everywhere
     private final int swearLevel;
 
     public SwearFilter(LandControl controller, int swearLevel) {
         this.controller = controller;
         this.swearLevel = swearLevel;
+        generateSwears();
     }
 
-    private static void generateSwears() {
+    private void generateSwears() {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(SWEAR_STREAM))) {
             br.lines().forEach(line -> {
                 String[] values = line.split(", ");
@@ -35,9 +36,9 @@ public final class SwearFilter {
         }
     }
 
-    private static String repeat(char c, int amount) {
+    private static String repeat(int amount) {
         char[] toReturn = new char[amount];
-        Arrays.fill(toReturn, c);
+        Arrays.fill(toReturn, SWEAR_REPLACE);
         return new String(toReturn);
     }
 
@@ -47,12 +48,9 @@ public final class SwearFilter {
     }
 
     public String replaceSwears(String message) {
-        if (swears.isEmpty())
-            generateSwears();
-
         String newMessage = message;
         for (String swear : swears) {
-            newMessage = newMessage.replaceAll("\\b(?i)" + swear + "\\b", repeat('♥', swear.length()));
+            newMessage = newMessage.replaceAll("\\b(?i)" + swear + "\\b", repeat(swear.length()));
         }
         return newMessage;
     }

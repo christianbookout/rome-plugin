6ee95b518076ee75f3ed5c40fbd05e99a51b18b2
intrnlerr <nulltestb1@gmail.com>
2022-01-27 06:18:35 +0000

    implement embeds

--- src/main/java/romeplugin/webhook/Webhooks.java
+++ src/main/java/romeplugin/webhook/Webhooks.java
@@ -1,7 +1,6 @@
 package romeplugin.webhook;
 
 import javax.net.ssl.HttpsURLConnection;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URL;
@@ -32,14 +31,32 @@ public class Webhooks {
             conn.addRequestProperty("Content-Type", "application/json");
             conn.addRequestProperty("User-Agent", "romeplugin");
             OutputStream output = conn.getOutputStream();
-            output.write(("{" + message + "}").getBytes(StandardCharsets.UTF_8));
+            output.write(("{\"content\":\"" + message + "\"}").getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void sendEmbeds(Collection<Embed> embeds) {
-
-        this.sendMessage("");
+        if (embeds.size() > 10) {
+            System.err.println("too many embeds!!");
+            return;
+        }
+        StringBuilder embedStr = new StringBuilder("[");
+        for (var embed : embeds) {
+            embedStr.append(embed.toJSON());
+        }
+        embedStr.append("]");
+        try {
+            HttpsURLConnection conn = (HttpsURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.addRequestProperty("Content-Type", "application/json");
+            conn.addRequestProperty("User-Agent", "romeplugin");
+            OutputStream output = conn.getOutputStream();
+            output.write(("{\"embeds\":\"" + embedStr + "\"}").getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

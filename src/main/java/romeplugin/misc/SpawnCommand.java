65fb294ba84104402f22fda839b13cf434694435
intrnlerr <nulltestb1@gmail.com>
2022-02-10 05:49:25 +0000

    only allow /spawn in the overworld
    fixes #14

--- src/main/java/romeplugin/misc/SpawnCommand.java
+++ src/main/java/romeplugin/misc/SpawnCommand.java
@@ -3,6 +3,7 @@ package romeplugin.misc;
 import org.bukkit.ChatColor;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,6 +23,11 @@ public class SpawnCommand implements CommandExecutor {
         if (!(sender instanceof Player)) return true;
         // TODO: some kind timer for teleporting
         var player = (Player) sender;
+        if (player.getWorld().getEnvironment() != World.Environment.NORMAL) {
+            // no teleporting in not the over world
+            player.sendMessage("NO MOVE IN NOT EARTH.");
+            return true;
+        }
         var pos = player.getWorld().getHighestBlockAt(landControl.getCenterX(), landControl.getCenterY());
         // have to add go up one otherwise player spawns in the ground
         var newLoc = pos.getLocation().add(0, 1, 0);

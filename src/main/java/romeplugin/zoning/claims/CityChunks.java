28d6bcc5f1c431eb44bde39b8beaf196bdd439fe
intrnlerr <nulltestb1@gmail.com>
2022-04-05 06:44:58 +0000

    start of poor chunk claiming implementation

---
+++ src/main/java/romeplugin/zoning/claims/CityChunks.java
@@ -0,0 +1,57 @@
+package romeplugin.zoning.claims;
+
+import romeplugin.database.SQLConn;
+
+import java.sql.SQLException;
+import java.util.OptionalInt;
+
+public class CityChunks {
+    CityChunks() {
+        makeTables();
+    }
+
+    private void makeTables() {
+        try (var conn = SQLConn.getConnection()) {
+            conn.prepareStatement("CREATE TABLE IF NOT EXISTS cityChunks(" +
+                    "cityId INT UNSIGNED NOT NULL," +
+                    "x INT NOT NULL," +
+                    "z INT NOT NULL);");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public OptionalInt getChunkOwner(int chunkX, int chunkZ) throws SQLException {
+        try (var conn = SQLConn.getConnection()) {
+            var stmt = conn.prepareStatement("SELECT cityId FROM cityChunks WHERE x=? AND z=?;");
+            stmt.setInt(1, chunkX);
+            stmt.setInt(2, chunkZ);
+            var res = stmt.executeQuery();
+            if (res.next()) {
+                return OptionalInt.of(res.getInt("cityId"));
+            }
+            return OptionalInt.empty();
+        }
+    }
+
+    public boolean rectIntersectsCity(int left, int top, int right, int bottom) {
+        try (var conn = SQLConn.getConnection()) {
+            var stmt = conn.prepareStatement("SELECT EXISTS(SELECT * FROM cityChunks WHERE" +
+                    "x >= ? AND" +
+                    "x <= ? AND" +
+                    "z >= ? AND" +
+                    "z <= ?);");
+            stmt.setInt(1, left);
+            stmt.setInt(2, right);
+            stmt.setInt(3, bottom);
+            stmt.setInt(4, top);
+            var res = stmt.executeQuery();
+            var hasNext = res.next();
+            assert hasNext;
+            return res.getBoolean(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return true;
+        }
+    }
+}
